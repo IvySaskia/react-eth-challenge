f4c6cc06490573cc320bd939a47d6b49a9fb4927
IvySaskia <ivysaskia@gmail.com>
2022-07-08 16:16:21 -0400

    Fix: Lint on getData.js function

--- src/utils/getData.js
+++ src/utils/getData.js
@@ -1,6 +1,8 @@
 const getData = (url) => {
   if (!url) return 'No url was provided';
-  return fetch(url).then((res) => res.json()).catch((error) => { console.error('There was an error!', error); });
+  return fetch(url)
+    .then((res) => res.json())
+    .catch((error) => { console.error('There was an error!', error); });
 };
 
 export default getData;
