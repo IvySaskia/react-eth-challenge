6f6a3350e1e1f0dac7ea70ad3891936e67eafb13
IvySaskia <ivysaskia@gmail.com>
2022-07-08 15:52:44 -0400

    Fix: Lint on project

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 
 class Header extends Component {
-    render() {
-        return (
-            <div>
-                <h1 className="Header-title">Header-title</h1>
-                <p className="Header-job-title">Header-job-title</p>
-                { this.props.children }
-            </div>
-        );
-    }
+  render() {
+    const { children } = this.props;
+
+    return (
+      <div>
+        <h1 className='Header-title'>Header-title</h1>
+        <p className='Header-job-title'>Header-job-title</p>
+        { children }
+      </div>
+    );
+  }
 }
 
 export default Header;
